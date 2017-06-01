d3a0ef4902f8cc535783f9e201ca5cb872dcfeeb
Francisco Jose de Lima Alves <francisco.alves@ltm.digital>
2017-06-01 00:54:02 -0300

    Capitulo 4

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,26 +1,27 @@
 class NegociacaoController {
-	
-	constructor(){
-		let $ = document.querySelector.bind(document);
-		
-		this._inputData = $('#data');
-		this._inputQuantidade = $('#quantidade');
-		this._inputValor = $('#valor');
-	
-	}
-	
-	adicionar(event){
-		event.preventDefault();
-		
-		
-		
-		let negociacao = new Negociacao(
-			this._inputData.value,
-			this._inputQuantidade.value,
-			this._inputValor.value
-		);
-		
-		console.log(negociacao);
-	}
-	
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+
+    }
+
+    adicionar(event) {
+        event.preventDefault();
+
+        let helper = new DateHelper();
+
+        let negociacao = new Negociacao(
+            helper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+        console.log(helper.dataParaTexto(negociacao.data));
+
+    }
 }
